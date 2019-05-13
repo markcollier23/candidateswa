fc35a3c215ccd61b4d7a6c0992b4f2b28c18e17d
Mark Collier <Mark Collier>
2019-05-14 00:00:02 +0800

    Size of image

--- public/javascripts/favourites.js
+++ public/javascripts/favourites.js
@@ -1,3 +1,5 @@
 document.write("\<h1\>Please select which set of candidates you would like to research.\<\/h1\>");
 node = img("https://raw.githubusercontent.com/21714487/pictures/master/buttons/yellow.PNG");
+node.style.height = 240;
+node.style.width = 840;
 textcentre("House of Representatives: Curtin",node);

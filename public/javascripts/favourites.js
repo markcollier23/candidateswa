e87bc5fbf9e10e33be24c609bfbd608466b7c26b
Mark Collier <Mark Collier>
2019-05-17 08:47:59 +0800

    Added Tangney.html

--- public/javascripts/favourites.js
+++ public/javascripts/favourites.js
@@ -1,2 +1,4 @@
 document.write("\<h1\>Please select which set of candidates you would like to research.\<\/h1\>");
 img("https://candidateswa.herokuapp.com/images/senate.png",url("https://candidateswa.herokuapp.com/senate.html"));
+img("https://candidateswa.herokuapp.com/images/curtin.png",url("https://candidateswa.herokuapp.com/curtin.html"));
+img("https://candidateswa.herokuapp.com/images/tangney.png",url("https://candidateswa.herokuapp.com/tangney.html"));

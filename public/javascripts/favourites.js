8342d6f6da0a3178c1df3ec7baa748f235cc82df
Mark Collier <Mark Collier>
2019-05-17 08:57:34 +0800

    Position the images

--- public/javascripts/favourites.js
+++ public/javascripts/favourites.js
@@ -1,6 +1,7 @@
 document.write("\<h1\>Please select which set of candidates you would like to research.\<\/h1\>");
 senate=img("https://candidateswa.herokuapp.com/images/senate.png",url("https://candidateswa.herokuapp.com/senate.html"));
+senate.style.top = "100px"
 curtin=img("https://candidateswa.herokuapp.com/images/curtin.png",url("https://candidateswa.herokuapp.com/curtin.html"));
-curtin.style.top = senate.style.top + 235;
+curtin.style.top = "335px";
 tangney=img("https://candidateswa.herokuapp.com/images/tangney.png",url("https://candidateswa.herokuapp.com/tangney.html"));
-tangney.style.top = curtin.style.top + 235;
+tangney.style.top = "570px";

6a734e1a217ada141eea2e41ebd395dd57b10f9b
Mark Collier <Mark Collier>
2019-05-17 08:53:46 +0800

    Position the images

--- public/javascripts/favourites.js
+++ public/javascripts/favourites.js
@@ -1,4 +1,6 @@
 document.write("\<h1\>Please select which set of candidates you would like to research.\<\/h1\>");
-img("https://candidateswa.herokuapp.com/images/senate.png",url("https://candidateswa.herokuapp.com/senate.html"));
-img("https://candidateswa.herokuapp.com/images/curtin.png",url("https://candidateswa.herokuapp.com/curtin.html"));
-img("https://candidateswa.herokuapp.com/images/tangney.png",url("https://candidateswa.herokuapp.com/tangney.html"));
+senate=img("https://candidateswa.herokuapp.com/images/senate.png",url("https://candidateswa.herokuapp.com/senate.html"));
+curtin=img("https://candidateswa.herokuapp.com/images/curtin.png",url("https://candidateswa.herokuapp.com/curtin.html"));
+curtin.style.top = senate.style.top + 235;
+tangney=img("https://candidateswa.herokuapp.com/images/tangney.png",url("https://candidateswa.herokuapp.com/tangney.html"));
+tangney.style.top = curtin.style.top + 235;

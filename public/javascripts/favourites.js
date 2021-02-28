9124b2c18acbdfb6448832bf34bc8a2ec718174f
Mark Alan Collier <m.collier@ibm.com>
2021-02-28 11:57:23 +0800

    Update favourites.js

--- public/javascripts/favourites.js
+++ public/javascripts/favourites.js
@@ -1,15 +1,11 @@
 document.write("\<h1\>Please select which set of candidates you would like to research.\<\/h1\>");
-assembly = document.createElement("H2");
-assembly.style.top = "200px";
-assembly.innerHTML = "Legislative Assembly Candidates";
-document.body.appendChild(assembly);
+assembly = img("https://candidateswa.herokuapp.com/WA2021/Legislative Assembly/name.png");
+assembly.style.top = "100px";
 churchlands=img("https://candidateswa.herokuapp.com/WA2021/Legislative Assembly/churchlands.png",url("https://candidateswa.herokuapp.com/WA2021/Legislative Assembly/churchlands.html"));
-churchlands.style.top = "300px"
+churchlands.style.top = "200px"
 wanneroo=img("https://candidateswa.herokuapp.com/WA2021/Legislative Assembly/wanneroo.png",url("https://candidateswa.herokuapp.com/WA2021/Legislative Assembly/wanneroo.png"));
-wanneroo.style.top = "600px";
-council = document.createElement("H2");
-council.style.top  = "700px";
-council.innerHTML = "Legislative Council Candidates";
-document.body.appendChild(council);
-north=img("https://candidateswa.herokuapp.com/WA2021/Legislative Council/North Metro.png",url("https://candidateswa.herokuapp.com/WA2021/Legislative Council/North Metro.html"));
-north.style.top = "800px";
+wanneroo.style.top = "400px";
+council = img("https://candidateswa.herokuapp.com/WA2021/Legislative Council/name.png");
+council.style.top = "500px";
+north=img("https://candidateswa.herokuapp.com/WA2021/Legislative Council/name.png",url("https://candidateswa.herokuapp.com/WA2021/Legislative Council/North Metro.html"));
+north.style.top = "600px";
